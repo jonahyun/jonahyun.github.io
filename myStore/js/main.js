4301439f24ccb7545e95c84303278d1823f054ec
Jona Hyun <jona.hyun@gmail.com>
2017-01-18 21:15:05 -0500

    figured out how to add images and added hover on button

--- myStore/js/main.js
+++ myStore/js/main.js
@@ -19,20 +19,31 @@ function showProducts(jsonObj) {
   var threeD = jsonObj['product'];
   
   for(i = 0; i < threeD.length; i++) {
+
     var myArticle = document.createElement('article');
+
+    //var myLink = document.createElement('a');
+    //myLink.setAttribute('href', threeD[i].link);
+    //Need to figure out how to wrap myArticle as anchor tag.
 
     var myPara1 = document.createElement('h3');
     var myPara2 = document.createElement('p');
     var myPara3 = document.createElement('p');
+    var myImg = document.createElement('img');
+    myImg.setAttribute('src',threeD[i].image);
+    myImg.setAttribute('width','210px');
+    myImg.setAttribute('height','140px');
 
     myPara1.textContent = threeD[i].name;
     myPara2.textContent = 'Author: ' + threeD[i].author;
     myPara3.textContent = 'Price: ' + threeD[i].price;
+    myImg.textContent = threeD[i].image;
 
+    myArticle.appendChild(myImg);
     myArticle.appendChild(myPara1);
     myArticle.appendChild(myPara2);
     myArticle.appendChild(myPara3);
-
+    
     section.appendChild(myArticle);
   }
 }
